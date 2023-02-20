98239a80c6e8e078e4b4886143a4d53e83d2713c
nadianielsen <U2404561@rts-365.dk>
2023-02-20 14:38:45 +0100

    Have started on the header and now looking for fonts

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import Navigation from "./Navigation";
+
+
+const Header = () => {
+    return ( 
+        <header className="w-full header-box-shadow flex justify-center items-center">
+            <Navigation />
+        </header>
+     );
+}
+ 
+export default Header;
