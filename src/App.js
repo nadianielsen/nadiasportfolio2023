557b91a33c05547cb48dd7d883529f81f31530fe
nadianielsen <U2404561@rts-365.dk>
2023-03-11 14:50:40 +0100

    Made the scrolltotop work with the router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
-import { Outlet } from "react-router-dom";
-import Footer from "./templates/Footer";
-import Header from "./components/Header";
+import Home from './pages/Home';
+import About from './pages/About';
+import Gallery from './pages/Gallery';
+import Contact from './pages/Contact';
+import Websites from './pages/Websites';
+import GraphicDesign from './pages/GraphicDesign';
+import { createRoutesFromElements, Route, createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from './components/Layout';
+
 
 function App() {
-  return (
-    <>
-      <Header />
-      <main className="row-start-2">
-        <Outlet />
-      </main>
-      <Footer />
-    </>
-  );
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Layout /> }>
+        <Route index element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/gallery' element={<Gallery />} />
+        <Route path='/websites' element={<Websites />} />
+        <Route path='/graphicdesign' element={<GraphicDesign />} />
+        <Route path='/contact' element={<Contact />} />
+      </Route>
+    )
+  )
+  return <RouterProvider router={router}/>
 }
 
 export default App;
---
+++ src/components/Layout.js
@@ -0,0 +1,19 @@
+import { Outlet } from "react-router-dom";
+import ScrollToTop from "../ScrollToTop";
+import Footer from "../templates/Footer";
+import Header from "./Header";
+
+function Layout() {
+    return (
+        <>
+        <ScrollToTop />
+            <Header />
+            <main className="row-start-2">
+                <Outlet />
+            </main>
+            <Footer />
+        </>
+    );
+}
+
+export default Layout;
