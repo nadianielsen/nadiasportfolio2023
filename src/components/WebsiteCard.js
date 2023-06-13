b44fb5224bd5fbf6ec4700fc9d49a358ef9bcd6f
nadianielsen <U2404561@rts-365.dk>
2023-06-13 12:28:17 +0200

    updated websites page

--- src/components/WebsiteCard.js
+++ src/components/WebsiteCard.js
@@ -4,8 +4,8 @@ import { Link } from "react-router-dom";
 const WebsiteCard = ({link, img, text}) => {
 
     return ( 
-        <div className="place-self-center w-[15rem] lg:w-[20rem] lg:h-[15rem] h-[20rem] bg-black rounded-2xl relative website-card-shadow">
-             <img src={img} alt="" className="object-cover absolute w-full h-full rounded-xl"/>
+        <div className="place-self-center w-[15rem] lg:w-[22rem] lg:h-[15rem] h-[20rem] bg-black rounded-2xl relative website-card-shadow">
+             <img src={img} alt="" className="absolute object-cover w-full h-full rounded-xl"/>
              <div className="w-full h-[30%] bg-neutral-100 bg-opacity-80 backdrop-blur-sm bottom-0 absolute rounded-xl flex justify-center items-center">
                 <Link className="hover:underline hover:font-semibold transition-all" to={link}>{text}</Link>
              </div>
