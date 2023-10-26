22c888f63dfd04e565836a040abdefe4dcf7a9b0
nadianielsen <U2404561@rts-365.dk>
2023-10-26 12:45:04 +0200

    made some links on about - smallcards component

--- src/pages/Design.js
+++ src/pages/Design.js
@@ -13,10 +13,18 @@ const Design = () => {
             <p>Since I have been taking the web developer education, I then have a plan on taking the media graphic designer(mediegrafiker) education, since I love digital design.</p>
             <p>I have used different apps for design, but my main graphic design app is procreate. In procreate you can both draw, animate, 3D design and customize pictures... and it's an app that is on ios only - iPad and iPhone.</p>
             <article className="grid gap-y-6 md:grid-cols-2 lg:grid-cols-4 place-items-center my-10">
-                <SmallCard name={"Procreate"} logo={"./images/procreate.png"} bgPrimary={"bg-gradient-to-t from-black/70 to-transparent"}/>
-                <SmallCard name={"Photoshop"} logo={"./images/photoshop.png"} bgPrimary={"bg-gradient-to-t from-[#6aa6f7]/70 to-transparent"}/>
-                <SmallCard name={"Picsart"} logo={"./images/picsart.jpg"} bgPrimary={"bg-gradient-to-t from-[#cf02bc]/50 to-transparent"}/>
-                <SmallCard name={"Canva"} logo={"./images/canva.png"} bgPrimary={"bg-gradient-to-t from-[#00c1cb]/70 to-transparent"}/>
+                <Link to={"https://procreate.com/"}>
+                    <SmallCard name={"Procreate"} logo={"./images/procreate.png"} bgPrimary={"bg-gradient-to-t from-black/70 to-transparent"}/>
+                </Link>
+                <Link to={"https://www.adobe.com/dk/products/photoshop.html"}>
+                    <SmallCard name={"Photoshop"} logo={"./images/photoshop.png"} bgPrimary={"bg-gradient-to-t from-[#6aa6f7]/70 to-transparent"}/>            
+                </Link>
+                <Link to={"https://picsart.com/"}>
+                    <SmallCard name={"Picsart"} logo={"./images/picsart.jpg"} bgPrimary={"bg-gradient-to-t from-[#cf02bc]/50 to-transparent"}/>          
+                </Link>
+                <Link to={"https://www.canva.com"}>
+                    <SmallCard name={"Canva"} logo={"./images/canva.png"} bgPrimary={"bg-gradient-to-t from-[#00c1cb]/70 to-transparent"}/>
+                </Link>
             </article>
             <p>I have designed some things in Photoshop, so I have some expertise in it.</p>
             <p>With picsart, was the app I started out on when I was interested in learning graphic design.</p>
