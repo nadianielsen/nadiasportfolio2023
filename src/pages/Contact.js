40e87455b02d7b105924714be60a2dd496d8c248
nadianielsen <U2404561@rts-365.dk>
2023-10-26 09:43:57 +0200

    fixing responsive on contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,16 +1,15 @@
 import { BsInstagram } from "react-icons/bs"
 import { FaLinkedin, FaFacebookSquare } from "react-icons/fa"
 import { SiGmail } from "react-icons/si";
-
 import { Link } from "react-router-dom";
 
 
 const Contact = () => {
     return ( 
-        <article className="h-[80vh] md:h-[60vh] lg:h-[90vh] grid w-full md:flex md:flex-row md:items-center relative">
+        <article className="h-[65vh] sm:h-[80vh] md:h-[60vh] lg:h-[90vh] grid w-full md:flex md:flex-row md:items-center relative">
             <img src="./images/contact-hero.jpg" alt="" className=" absolute object-cover w-full h-full -z-10 opacity-70"/>
-            <h2 className="uppercase text-5xl md:text-6xl lg:text-9xl m-auto md:m-0 px-10 md:pl-14 lg:pl-40 lg:w-[50%] font-light">Let's get <span className="lg:pl-32">in touch</span></h2>
-            <article className="border-y-2 border-black w-[35%] h-[20rem] grid grid-cols-1 grid-rows-4 m-auto md:m-0">
+            <h2 className="uppercase text-3xl sm:text-5xl md:text-6xl lg:text-9xl m-auto md:m-0 px-10 md:pl-14 lg:pl-40 lg:w-[50%] font-light">Let's get <span className="lg:pl-32">in touch</span></h2>
+            <article className="border-y-2 border-black sm:w-[35%] h-[20rem] grid grid-cols-1 grid-rows-4 m-auto md:m-0">
                 <div className="flex justify-center items-center gap-x-3">
                     <BsInstagram className="md:text-2xl lg:text-4xl" />
                 <div className="flex flex-col md:block">
@@ -19,13 +18,13 @@ const Contact = () => {
                 </div>
                 </div>
                 <div className="flex items-center justify-center gap-x-3 row-start-2 mr-2">
-                    <FaLinkedin className="text-2xl lg:text-4xl"/>
+                    <FaLinkedin className="sm:text-2xl lg:text-4xl"/>
                 <div className="">
                         <Link to={"https://www.linkedin.com/in/nadia-nielsen-b313b6222/"} className="pt-6 font- hover:text-sky-600 transition-all text-sm md:text-base">Linkedin</Link> 
                 </div>
                 </div>
                     <div className="flex items-center justify-center row-start-3 gap-x-3">
-                    <FaFacebookSquare className="text-2xl lg:text-4xl"/>
+                    <FaFacebookSquare className="sm:text-2xl lg:text-4xl"/>
                         <div>
                             <Link to={"https://www.facebook.com/nadia.nielsen.5817300/"} className="hover:text-blue-900 transition-all text-sm md:text-base ">FaceBook</Link>
                         </div>
