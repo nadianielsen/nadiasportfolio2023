a7c19f3cd7b91ba6781b50744a6910ff075bc5fa
nadianielsen <U2404561@rts-365.dk>
2023-11-06 12:36:37 +0100

    responsive with font-size & border-b on home animation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,10 +8,10 @@ const Home = () => {
             <div className="w-full h-[60vh] md:h-screen  flex justify-center items-center">
                 <div className="w-[80vw] lg:w-[50vw] bg-neutral-100 py-20 md:h-[40vh] lg:h-[60vh] lg:py-0 bg-opacity-50 absolute backdrop-blur-sm rounded-xl grid">
                     <motion.div initial={{ x: 200 }} animate={{ x: 0 }} transition={{ duration: 0.50 }} className="overflow-hidden self-end justify-self-center text-left">
-                        <motion.p className="home-text font-semibold w-80 pl-16 text-xl md:w-auto md:pl-0 md:text-5xl lg:text-7xl uppercase text-white text-shadow" initial={{ x: 100 }} animate={{ x: 0 }} transition={{ duration: 0.60 }}>Welcome to my portfolio</motion.p>
+                        <motion.p className="home-text font-semibold w-80 pl-16 text-xl md:w-auto md:pl-0 md:text-5xl size1200:6xl size1470:text-7xl uppercase text-white text-shadow" initial={{ x: 100 }} animate={{ x: 0 }} transition={{ duration: 0.60 }}>Welcome to my portfolio</motion.p>
                     </motion.div>
                     <motion.div initial={{ x: -200 }} animate={{ x: 0 }} transition={{ duration: 0.50 }} className="overflow-hidden justify-center">
-                        <div className="border-b-[0.18rem] mx-6 lg:mx-10 border-white"></div>
+                        <div className="border-b-[0.18rem] mx-6 size1470:mx-10 border-white"></div>
                     </motion.div>
                 </div>
                 <img src="./images/hero.jpg" alt="" className="object-cover w-full h-full" />
