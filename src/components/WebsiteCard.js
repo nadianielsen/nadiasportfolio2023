fd1f0b9f5184bb2555081d06b6a077a8ec59bcb0
nadianielsen <U2404561@rts-365.dk>
2023-11-10 13:53:08 +0100

    editing the hover effect on WebsiteCard.js

--- src/components/WebsiteCard.js
+++ src/components/WebsiteCard.js
@@ -5,9 +5,9 @@ const WebsiteCard = ({ link, img, text }) => {
 
    return (
       <div className="place-self-center w-[20.5rem] h-[15rem] bg-black rounded-2xl relative website-card-shadow">
-         <img src={img} alt="" className="absolute object-cover w-full h-full rounded-xl"/>
-         <div className="w-full h-[30%] bg-neutral-100 bg-opacity-80 backdrop-blur-sm bottom-0 absolute rounded-xl flex justify-center items-center">
-            <Link className="hover:underline hover:font-semibold transition-all text-[0.92rem] lg:text-base" to={link}>{text}</Link>
+         <img src={img} alt="" className="absolute object-cover w-full h-full rounded-xl" />
+         <div className="group hover:transition-colors duration-200 delay-150 hover:bg-neutral-300/80 w-full h-[30%] bg-neutral-100/80 backdrop-blur-sm bottom-0 absolute rounded-xl flex justify-center items-center">
+            <Link className="text-[0.92rem] lg:text-base group-hover:underline transition duration-200 delay-250" to={link}>{text}</Link>
          </div>
       </div>
    );
