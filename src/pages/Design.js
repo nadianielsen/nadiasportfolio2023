65df60f133ed49ae03c392e987bd80b7a2306d99
nadianielsen <U2404561@rts-365.dk>
2024-04-11 15:13:36 +0200

    fixed grammar errors on aboutme, contact & design.js

--- src/pages/Design.js
+++ src/pages/Design.js
@@ -4,44 +4,45 @@ import SmallCard from "../components/SmallCard";
 
 
 const Design = () => {
-    return ( 
+    return (
         <>
-        <PagesLinks page={"Design"} prevPage={"About"} location={"/design"}/>
-        <article className="grid mx-20 gap-y-2.5 my-6">
-            <h1 className="text-xl font-semibold">Design</h1>
-            <h2 className="text-lg font-medium underline">Since I have passion for design, I also want to pursue the graphic design education</h2>
-            <p>Since I have been taking the web developer education, I then have a plan on taking the media graphic designer(mediegrafiker) education, since I love digital design.</p>
-            <p>I have used different apps for design, but my main graphic design app is procreate. In procreate you can both draw, animate, 3D design and customize pictures... and it's an app that is on ios only - iPad and iPhone.</p>
-            <article className="grid gap-y-6 md:grid-cols-2 lg:grid-cols-3 place-items-center my-10">
-                <Link to={"https://procreate.com/"}>
-                    <SmallCard name={"Procreate"} logo={"./images/procreate.png"} bgPrimary={"bg-gradient-to-t from-black/70 to-transparent"}/>
-                </Link>
-                <Link to={"https://picsart.com/"}>
-                    <SmallCard name={"Picsart"} logo={"./images/picsart.jpg"} bgPrimary={"bg-gradient-to-t from-[#cf02bc]/50 to-transparent"}/>          
-                </Link>
-                <Link to={"https://www.canva.com"}>
-                    <SmallCard name={"Canva"} logo={"./images/canva.png"} bgPrimary={"bg-gradient-to-t from-[#00c1cb]/70 to-transparent"}/>
-                </Link>
-                <Link to={"https://www.adobe.com/products/photoshop.html"}>
-                    <SmallCard name={"Photoshop"} logo={"./images/photoshop.png"} bgPrimary={"bg-gradient-to-t from-[#6aa6f7]/70 to-transparent"}/>            
-                </Link>
-                <Link to={"https://www.adobe.com/products/illustrator.html"}>
-                    <SmallCard name={"Illustrator"} logo={"./images/illustrator.png"} bgPrimary={"bg-gradient-to-t from-[#fe9900]/70 to-transparent"}/>
-                </Link>
-                <Link to={"https://www.adobe.com/products/indesign.html"}>
-                    <SmallCard name={"InDesign"} logo={"./images/indesign.png"} bgPrimary={"bg-gradient-to-t from-[#de4868]/70 to-transparent"}/>
-                </Link>
+            <PagesLinks page={"Design"} prevPage={"About"} location={"/design"} />
+            <article className="grid mx-20 gap-y-2.5 my-6">
+                <h1 className="text-xl font-semibold">Design</h1>
+                <h2 className="text-lg font-medium underline">Since I have passion for design, I also want to pursue the graphic design education</h2>
+                <p>I am currently taking the media graphic designer education because I love the digital world.</p>
+                <p>I have used different apps for design, but my main graphic design app is procreate. In procreate you can both draw, animate, 3D design and customize pictures... and it's an app that is on ios only - iPad and iPhone.</p>
+                <p>The education I am taking right now, I have learned more about the mainly used adobe programs and now have more experience.</p>
+                <article className="grid gap-y-6 md:grid-cols-2 lg:grid-cols-3 place-items-center my-10">
+                    <Link to={"https://procreate.com/"}>
+                        <SmallCard name={"Procreate"} logo={"./images/procreate.png"} bgPrimary={"bg-gradient-to-t from-black/70 to-transparent"} />
+                    </Link>
+                    <Link to={"https://picsart.com/"}>
+                        <SmallCard name={"Picsart"} logo={"./images/picsart.jpg"} bgPrimary={"bg-gradient-to-t from-[#cf02bc]/50 to-transparent"} />
+                    </Link>
+                    <Link to={"https://www.canva.com"}>
+                        <SmallCard name={"Canva"} logo={"./images/canva.png"} bgPrimary={"bg-gradient-to-t from-[#00c1cb]/70 to-transparent"} />
+                    </Link>
+                    <Link to={"https://www.adobe.com/products/photoshop.html"}>
+                        <SmallCard name={"Photoshop"} logo={"./images/photoshop.png"} bgPrimary={"bg-gradient-to-t from-[#6aa6f7]/70 to-transparent"} />
+                    </Link>
+                    <Link to={"https://www.adobe.com/products/illustrator.html"}>
+                        <SmallCard name={"Illustrator"} logo={"./images/illustrator.png"} bgPrimary={"bg-gradient-to-t from-[#fe9900]/70 to-transparent"} />
+                    </Link>
+                    <Link to={"https://www.adobe.com/products/indesign.html"}>
+                        <SmallCard name={"InDesign"} logo={"./images/indesign.png"} bgPrimary={"bg-gradient-to-t from-[#de4868]/70 to-transparent"} />
+                    </Link>
+                </article>
+                <p>Gotten more experience in Photoshop, Illustrator, Indesign...</p>
+                <p>With picsart, was the app I started out on when I was interested in learning graphic design.</p>
+                <p>I am just starting out on Canva, trying to learn it since it has become very popular these past few years.</p>
+                <article>
+                    <h2 className="text-xl font-medium underline py-2">Link to the gallery to see my designs</h2>
+                    <Link to={"/graphicdesign"} className="hover:text-[#f03fa9] underline transition-all">Graphic design</Link>
+                </article>
             </article>
-            <p>I am taking the media graphic design education right now and I am for example Photoshop, Illustrator and InDesign.</p>
-            <p>With picsart, was the app I started out on when I was interested in learning graphic design.</p>
-            <p>I am just starting out on Canva, trying to learn it since it has become very popular these past few years.</p>
-            <article>
-                <h2 className="text-xl font-medium underline py-2">Link to the gallery to see my designs</h2>
-                <Link to={"/graphicdesign"} className="hover:text-[#f03fa9] underline transition-all">Graphic design</Link>
-            </article>
-        </article>
         </>
-     );
+    );
 }
- 
+
 export default Design;
