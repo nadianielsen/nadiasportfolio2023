5b6437af01703e76640cabbe1969cd30e1beaa6e
nadianielsen <U2404561@rts-365.dk>
2024-06-26 19:22:55 +0200

    Gallery updated with a new page with school design projects

--- src/components/LinkCards.js
+++ src/components/LinkCards.js
@@ -1,16 +1,14 @@
 import { Link } from "react-router-dom";
 
 
-const LinkCards = ({url, img, subscription, heading, alt}) => {
+const LinkCards = ({url, img, subscription, heading, alt, bgGradient}) => {
     return ( 
-        <article className="bg-neutral-200 w-[19rem] size400:w-[20rem] h-[25rem] rounded-3xl shadow-lg">
-             <div className="relative w-full h-full group">
-                <div className="bg-neutral-100 bg-opacity-30 w-full h-36 rounded-b-3xl px-4 pt-4 absolute bottom-0 ">
+        <article className={`${bgGradient} w-[19rem] size400:w-[20rem] h-[25rem] rounded-3xl shadow-lg relative`}>
+                <div className="bg-neutral-100 bg-opacity-30 backdrop-blur-[1px] w-full h-36 rounded-b-3xl px-4 pt-4 absolute bottom-0 ">
                     <Link to={url} className="text-white text-2xl font-medium transition-all hover:text-4xl hover:transition-all hover:duration-400 hover:delay-75 white-shadow">{heading}</Link>
                      <p className="text-white white-shadow">{subscription}</p>
                 </div>
-            <img src={img} alt={alt} className="object-cover rounded-3xl w-full h-full" />
-        </div>
+            <img src={img} alt={alt} className={`object-cover rounded-3xl w-full h-full absolute -z-10`} />
         </article>
      );
 }
