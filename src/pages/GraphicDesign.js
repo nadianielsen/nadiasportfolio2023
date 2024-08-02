cf7e900ff29f940744902a5aca369ed4d3d99b84
nadianielsen <U2404561@rts-365.dk>
2024-08-02 08:43:59 +0200

    fixed some text on graphic design cards

--- src/pages/GraphicDesign.js
+++ src/pages/GraphicDesign.js
@@ -6,12 +6,12 @@ const GraphicDesign = () => {
 
     return (
         <>
-        <article className="grid gap-y-20 my-14 size1200:grid-cols-3 justify-items-center m-auto lg:w-[80%] lg:gap-x-20 ">
-            <LinkCards url={"/albums"} subscription={"Here is my album covers of musicians and drawings"} heading={"Albums & Drawings"} img={"./images/albums/The Weeknd - Playlist Cover - Red.jpg"} alt={"Albums - Page - Link"} />
-            <LinkCards bgGradient={"bg-gradient-to-b from-transparent via-transparent to-black/60"} url={"/schoolprojects"} subscription={"The school projects I am most proud of "} heading={"School Projects"} img={"./images/school/Odense Chokoladehus - forside folder.jpg"} alt={"Drawings - Page - Link"} />
-            <LinkCards bgGradient={"bg-gradient-to-b from-transparent via-transparent to-black/60"} url={"/posters"} subscription={"Here is my posters and wallpapers of influencer and celebrities, etc..."} heading={"Posters and wallpapers"} img={"./images/posters-wallpapers/Poster-Album cover - YOU - 27-10-2023.jpg"} alt={"Posters and Wallpapers - Page - Link"} />
-        </article>
-        <h1 className="flex justify-center py-10 font-semibold underline size200:mx-10 size866:00:mx-0">Sources/items that are used for my designs are personal use sources from the internet, just with some of my own touch ups to create posters or something else.</h1>
+            <article className="grid gap-y-20 my-14 size1200:grid-cols-3 justify-items-center m-auto lg:w-[80%] lg:gap-x-20 ">
+                <LinkCards url={"/albums"} subscription={"Here is my album covers of musicians and drawings"} heading={"Albums & Drawings"} img={"./images/albums/The Weeknd - Playlist Cover - Red.jpg"} alt={"Albums - Page - Link"} />
+                <LinkCards bgGradient={"bg-gradient-to-b from-transparent via-transparent to-black/60"} url={"/schoolprojects"} subscription={"The school projects I am most proud of "} heading={"School Projects"} img={"./images/school/Odense Chokoladehus - forside folder.jpg"} alt={"Drawings - Page - Link"} />
+                <LinkCards bgGradient={"bg-gradient-to-b from-transparent via-transparent to-black/60"} url={"/posters"} subscription={"Here is my posters and wallpapers of actors and celebrities, etc..."} heading={"Posters and wallpapers"} img={"./images/posters-wallpapers/Poster-Album cover - YOU - 27-10-2023.jpg"} alt={"Posters and Wallpapers - Page - Link"} />
+            </article>
+            <h1 className="flex justify-center py-10 font-semibold underline size200:mx-10 size866:00:mx-0">Sources/items that are used for my designs are personal use sources from the internet, just with some of my own touch ups to create posters or something else.</h1>
         </>
     );
 }
