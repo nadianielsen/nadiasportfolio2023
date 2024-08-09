1489f5ef8900895936e2ece69d80085412d6b8d9
nadianielsen <U2404561@rts-365.dk>
2024-08-09 11:34:52 +0200

    changed a few things...

--- src/pages/Posters.js
+++ src/pages/Posters.js
@@ -86,13 +86,13 @@ const Posters = () => {
 
     return (
         <>
-            <PagesLinks prevPage={"Graphic Design"} location={"/posters"} page={"Posters and Wallpapers"} />
+            <PagesLinks prevPage={"Graphic Design"} location={"/posters"} page={"Posters & etc..."} />
             <article className="grid grid-cols-1 md:grid-cols-2 size1470:grid-cols-3 justify-items-center w-[75%] m-auto my-10 size300:gap-x-20 gap-12">
                 {posterswallpapers.map(({ img, headline, text }) => (
                     // <GalleryImg img={img} headline={headline} text={text} alt={headline} height={"[30rem]"} mdheight={"[32rem]"} />
                     // <img src={img} alt={img} className="rounded-2xl" />
                     <ContentWrapper key={text} content={
-                        <div className="w-[20rem] h-[26rem] lg:w-[24rem] lg:h-[34rem] group [perspective:1000px] drop-shadow-md">
+                        <div className="w-[20rem] h-[28rem] lg:w-[24rem] lg:h-[34rem] group [perspective:1000px] drop-shadow-md">
                             <div className="relative h-full w-full transition-all duration-500 [transform-style:preserve-3d] group-hover:[transform:rotateY(180deg)]">
                                 <div className="absolute inset-0">
                                     <img className="rounded-2xl w-full h-full object-cover" src={img} alt={headline} />
