04093f6fa4ef2551cd7fc244b89c046f80754a28
nadianielsen <U2404561@rts-365.dk>
2024-08-26 10:09:43 +0200

    maximum on width and height

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -6,7 +6,7 @@ import AgeFunction from "../components/AgeFunction";
 const Home = () => {
     return (
         <article className="grid z-50">
-            <div className="w-full h-[60vh] size400:h-screen flex justify-center items-center">
+            <div className="max-w-full max-h-[60vh] size400:max-h-screen flex justify-center items-center">
                 <div className="w-[80vw] lg:w-[50vw] bg-neutral-100 py-20 md:h-[30vh] size1470:h-[60vh] lg:py-0 bg-opacity-50 absolute backdrop-blur-[2px] drop-shadow-md rounded-xl grid">
                     <motion.div initial={{ x: 200 }} animate={{ x: 0 }} transition={{ duration: 0.50 }} className="overflow-hidden self-end justify-self-center text-left">
                         <motion.p className="home-text font-semibold w-80 pl-16 text-xl md:w-auto md:pl-0 md:text-5xl size1200:6xl size1470:text-7xl uppercase text-white text-shadow" initial={{ x: 100 }} animate={{ x: 0 }} transition={{ duration: 0.60 }}>Welcome to my portfolio</motion.p>
@@ -15,7 +15,7 @@ const Home = () => {
                         <div className="border-b-[0.18rem] mx-6 size1470:mx-10 border-white"></div>
                     </motion.div>
                 </div>
-                <img src="./images/website banner copy.webp" alt="website banner" sizes="width: 1920px, height: 50vh" className="object-fill w-full h-full" />
+                <img src="./images/website banner copy.webp" alt="website banner" className="object-fill w-full h-full" />
             </div>
             <article className="place-self-center my-6"> 
                 <h2 className="font-semibold text-center text-3xl py-4 underline">Video portfolio</h2>
