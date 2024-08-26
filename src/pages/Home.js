f544b2ed1231a4f42cc80f49b1223a597efee279
nadianielsen <U2404561@rts-365.dk>
2024-08-26 09:35:17 +0200

    changed picture of me into a webp file instead of a png

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -22,7 +22,7 @@ const Home = () => {
                 <video src="Nadia Nielsen - Video portfolio.mp4" controls className="w-[70vw] lg:w-[40vw] border-2" poster="video thumbnail.png"></video>
             </article>
             <article className="w-full h-[80vh] sm:h-screen grid md:grid-cols-2">
-                <img src="./images/me - photo 2.png" alt="" className="w-[30rem] h-[25rem] size866:h-[35rem] lg:mb-10 place-self-center object-cover" />
+                <img src="./images/me - photo 2.webp" alt="" className="w-[30rem] h-[25rem] size866:h-[35rem] lg:mb-10 place-self-center object-cover" />
                 <div className="col-start-2 row-start-1 border-r-2 w-2 h-40 border-black self-center hidden md:block"></div>
                 <div className="w-[58%] sm:w-[53%] place-self-center md:col-start-2 md:row-start-1 row-start-2 pb-20 size700:pb-0 lg:py-4">
                     <p className="sm:text-xl ">Hello and welcome. <AgeFunction />  I come from Denmark and I am taking the media graphic design education.</p>
