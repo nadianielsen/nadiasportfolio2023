f0a1f757a363d9b37dd860458e5a4ac6f0b7ef75
nadianielsen <U2404561@rts-365.dk>
2024-09-23 09:29:52 +0200

    organized pictures for about into folders

--- src/pages/Design.js
+++ src/pages/Design.js
@@ -14,12 +14,12 @@ const Design = () => {
                 <p>The graphic design programs that I use is, Procreate, Photoshop, Illustrator and InDesign.</p>
                 <p>I am getting most of my experience in the Adobe program is by my education.</p>
                 <article className="grid gap-y-10 md:grid-cols-2 lg:grid-cols-3 my-10 size1100:w-[60vw] place-items-center size1100:my-8 size1100:m-auto">
-                    <SmallCard link={"https://procreate.com/"} name={"Procreate"} logo={"./images/procreate.webp"} bgHover={"group-hover:bg-black/70"} bgPrimary={"bg-gradient-to-t from-black/70 to-transparent"} />
-                    <SmallCard link={"https://picsart.com/"} name={"Picsart"} logo={"./images/picsart.webp"} bgHover={"group-hover:bg-[#cf02bc]/70"} bgPrimary={"bg-gradient-to-t from-[#cf02bc]/50 to-transparent"} />
-                    <SmallCard link={"https://www.canva.com"} name={"Canva"} logo={"./images/canva.webp"} bgHover={"group-hover:bg-[#00c1cb]/70"} bgPrimary={"bg-gradient-to-t from-[#00c1cb]/70 to-transparent"} />
-                    <SmallCard link={"https://www.adobe.com/products/photoshop.html"} name={"Photoshop"} bgHover={"group-hover:bg-[#6aa6f7]/70"} logo={"./images/photoshop.webp"} bgPrimary={"bg-gradient-to-t from-[#6aa6f7]/70 to-transparent"} />
-                    <SmallCard link={"https://www.adobe.com/products/illustrator.html"} name={"Illustrator"} bgHover={"group-hover:bg-[#fe9900]/70"} logo={"./images/illustrator.webp"} bgPrimary={"bg-gradient-to-t from-[#fe9900]/70 to-transparent"} />
-                    <SmallCard link={"https://www.adobe.com/products/indesign.html"} name={"InDesign"} logo={"./images/indesign.webp"} bgHover={"group-hover:bg-[#de4868]/70"} bgPrimary={"bg-gradient-to-t from-[#de4868]/70 to-transparent"} />
+                    <SmallCard link={"https://procreate.com/"} name={"Procreate"} logo={"./images/design programs/procreate.webp"} bgHover={"group-hover:bg-black/70"} bgPrimary={"bg-gradient-to-t from-black/70 to-transparent"} />
+                    <SmallCard link={"https://picsart.com/"} name={"Picsart"} logo={"./images/design programs/picsart.webp"} bgHover={"group-hover:bg-[#cf02bc]/70"} bgPrimary={"bg-gradient-to-t from-[#cf02bc]/50 to-transparent"} />
+                    <SmallCard link={"https://www.canva.com"} name={"Canva"} logo={"./images/design programs/canva.webp"} bgHover={"group-hover:bg-[#00c1cb]/70"} bgPrimary={"bg-gradient-to-t from-[#00c1cb]/70 to-transparent"} />
+                    <SmallCard link={"https://www.adobe.com/products/photoshop.html"} name={"Photoshop"} bgHover={"group-hover:bg-[#6aa6f7]/70"} logo={"./images/design programs/photoshop.webp"} bgPrimary={"bg-gradient-to-t from-[#6aa6f7]/70 to-transparent"} />
+                    <SmallCard link={"https://www.adobe.com/products/illustrator.html"} name={"Illustrator"} bgHover={"group-hover:bg-[#fe9900]/70"} logo={"./images/design programs/illustrator.webp"} bgPrimary={"bg-gradient-to-t from-[#fe9900]/70 to-transparent"} />
+                    <SmallCard link={"https://www.adobe.com/products/indesign.html"} name={"InDesign"} logo={"./images/design programs/indesign.webp"} bgHover={"group-hover:bg-[#de4868]/70"} bgPrimary={"bg-gradient-to-t from-[#de4868]/70 to-transparent"} />
                 </article>
                 <p>I have a minimum experience in AfterEffects & Premiere Pro...</p>
                 <p>With picsart, was the app I started out on when I was interested in learning graphic design.</p>
