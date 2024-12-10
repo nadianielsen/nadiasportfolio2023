56fe4787c518f88af820ed847df43230f4fb9f87
nadianielsen <U2404561@rts-365.dk>
2024-12-10 13:04:53 +0100

    redirect moved

--- src/components/HomeFirstLook.js
+++ src/components/HomeFirstLook.js
@@ -62,15 +62,15 @@ const HomeFirstLook = () => {
             <section className=" lg:ml-16 size1400:ml-0 size1400:mr-40 flex flex-col lg:flex-row justify-center items-center">
                 <motion.img initial={{opacity: 0}} animate={{opacity: 1}} transition={{delay: 0.1}} className="size1200:relative size1200:bottom-28 size1400:bottom-[20rem] size1700:bottom-[18rem] w-[15rem] lg:w-[20.5rem] border-2 border-black rounded-tr-[3rem] rounded-bl-[3rem]" src="./images/me copy.webp" alt="me - nadia nielsen" />
                 <motion.div initial={{opacity: 0}} animate={{opacity: 1}} transition={{ delay: 0.25}} className="size1200:relative size1200:bottom-28 size1400:bottom-[16rem] size1700:bottom-[14rem] py-12 px-16 size-1200:px-[25rem] size1400:py-32 flex flex-col">
-                    <h2 className="text-3xl size1400:text-[2.5rem]">Hello and <span className="crimson-font text-[#00A5E7]">welcome</span></h2>
+                    <h2 className="text-3xl size1400:text-[2.5rem]">Hello and <span className="crimson-font text-[#56d64f]">welcome</span></h2>
                     <p className="size1200:w-[26rem] size1200:text-xl">My name is Nadia Nielsen, I am {age} years old and I come from Zealand in Denmark. I am currently taking the media graphic designer education (mediegrafiker uddannelsen).</p>
                     <figure className="w-[27.5 rem] h-2 border-b-2 border-black py-2"></figure>
-                    <p className="text-lg pt-2.5">On my website you can browse through my artworks on the <Link to={"/gallery"} className="underline hover:text-[#00A5E7] hover:font-semibold hover:duration-500 hover:ease-in-out hover:transition-colors">gallery page.</Link></p>
-                    <Link to={"/"} className="w-[15rem] text-lg flex gap-x-1 items-center underline hover:no-underline hover:text-[#00A5E7] hover:duration-500 hover:transition-colors hover:ease-in-out hover:font-semibold">
+                    <p className="text-lg pt-2.5">On my website you can browse through my artworks on the <Link to={"/gallery"} className="underline hover:text-[#56d64f] hover:font-semibold hover:duration-500 hover:ease-in-out hover:transition-colors">gallery page.</Link></p>
+                    <Link to={"/"} className="w-[15rem] text-lg flex gap-x-1 items-center underline hover:no-underline hover:text-[#56d64f] hover:duration-500 hover:transition-colors hover:ease-in-out hover:font-semibold">
                         <p>For more knowledge about me</p>
                         <IoIosArrowRoundForward className="text-4xl mt-1" />
                     </Link>
-                    <motion.div initial={{ opacity: 0, y: [ -40, 40, 0] }} whileInView={{ opacity: 1, y: [-20, 20, 0] }} transition={{ delay: 2.2, duration: 0.40, repeatDelay: 2, repeat: Infinity }} className="w-12 h-12 size1400:w-16 size1400:h-16 m-auto mt-10 lg:mt-24 size1400:mt-12 bg-[#00A5E7] flex justify-center items-center rounded-full">
+                    <motion.div initial={{ opacity: 0, y: [ -40, 40, 0] }} whileInView={{ opacity: 1, y: [-20, 20, 0] }} transition={{ delay: 2.2, duration: 0.40, repeatDelay: 2, repeat: Infinity }} className="w-12 h-12 size1400:w-16 size1400:h-16 m-auto mt-10 lg:mt-24 size1400:mt-12 bg-[#56d64f] flex justify-center items-center rounded-full">
                         <IoIosArrowDown className="text-3xl text-white mt-1" />
                     </motion.div>
                 </motion.div>
