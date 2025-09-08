6d9f195085f65ce4c6ad4764b1a584a7cd4ec6f5
nadia <nadiamarie2111@gmail.com>
2025-09-08 14:09:20 +0200

    layout

--- src/components/DescriptionArt.js
+++ src/components/DescriptionArt.js
@@ -1,12 +1,12 @@
 
 
-const DescriptionArt = ({heading, text}) => {
-    return ( 
+const DescriptionArt = ({ heading, text }) => {
+    return (
         <section className="md:px-[7.5rem] size866:px-28 lg:px-28 size1560:px-[29rem] pt-10 w-[20rem] md:w-full m-auto md:m-0 size1560:text-[1.02rem] ">
             <h2 className="font-semibold">{heading}</h2>
             {text}
         </section>
-     );
+    );
 }
- 
+
 export default DescriptionArt;
--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -11,7 +11,7 @@ const Gallery = () => {
             </article> */}
             <>
                 {/* <PagesLinks margin={"mt"} prevPage={"Gallery"} location={"/graphicdesign"} page="Graphic Design" /> */}
-                <article className="grid gap-y-20 my-14 size1200:grid-cols-3 justify-items-center m-auto lg:w-[80%] size1560:w-[60%] lg:gap-x-20 lg:my-32">
+                <article className="grid gap-y-20 mb-14 size1200:grid-cols-3 justify-items-center m-auto lg:w-[80%] size1560:w-[60%] lg:gap-x-20 lg:my-32">
                     <LinkCards url={"/albums"} subscription={"Here is my album covers of musicians and drawings"} heading={"Albums & Drawings"} img={"./images/albums/The Weeknd - Playlist Cover - Red.webp"} alt={"Albums - Page - Link"} />
                     <LinkCards bgGradient={"bg-gradient-to-b from-transparent via-transparent to-black/60"} url={"/schoolprojects"} subscription={"The school projects I am most proud of "} heading={"School Projects"} img={"./images/school/Roskilde - fjorland plakat.webp"} alt={"Drawings - Page - Link"} />
                     <LinkCards bgGradient={"bg-gradient-to-b from-transparent via-transparent to-black/60"} url={"/posters"} subscription={"Here is my posters and wallpapers of actors and celebrities, etc..."} heading={"Posters & etc..."} img={"./images/posters-wallpapers/Mysterious poster - purple gradient - 7-8-2024.webp"} alt={"Posters and Wallpapers - Page - Link"} />
