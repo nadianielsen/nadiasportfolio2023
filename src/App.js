8949861c997fba9e85ef02aecff53bed1802989b
nadia <nadiamarie2111@gmail.com>
2025-09-08 13:50:54 +0200

    // import Websites from './pages/Websites'; in app.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Gallery from './pages/Gallery';
 import Contact from './pages/Contact';
-import Websites from './pages/Websites';
+// import Websites from './pages/Websites';
 import GraphicDesign from './pages/GraphicDesign';
 import { createRoutesFromElements, Route, createBrowserRouter, RouterProvider } from "react-router-dom";
 import Layout from './components/Layout';
