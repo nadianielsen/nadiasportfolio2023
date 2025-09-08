785efe9e26825020bf1a3e8d33d92d3f64ad0fea
nadia <nadiamarie2111@gmail.com>
2025-09-08 14:19:29 +0200

    changed picture in poster link

--- src/components/LinkCards.js
+++ src/components/LinkCards.js
@@ -1,16 +1,16 @@
 import { Link } from "react-router-dom";
 
 
-const LinkCards = ({url, img, subscription, heading, alt, bgGradient}) => {
-    return ( 
-        <Link to={url} className={`${bgGradient} w-[15rem] size300:w-[19rem] size400:w-[20rem] h-[25rem] size1560:w-[25rem] size1560:h-[30rem] rounded-3xl shadow-lg relative`}>
-                <div className="bg-neutral-100 bg-opacity-30 backdrop-blur-[1.5px] w-full h-36 rounded-b-3xl px-4 pt-4 absolute bottom-0 ">
-                    <h2 className="text-white text-2xl font-medium transition-all hover:text-4xl hover:transition-all hover:duration-400 hover:ease-in-out drop-shadow-linkCards">{heading}</h2>
-                     <p className="text-white drop-shadow-linkCards">{subscription}</p>
-                </div>
+const LinkCards = ({ url, img, subscription, heading, alt, bgGradient }) => {
+    return (
+        <Link to={url} className={`${bgGradient} w-[15rem] size300:w-[19rem] size400:w-[20rem] h-[25rem] size1560:w-[25rem] size1560:h-[32rem] rounded-3xl shadow-lg relative`}>
+            <div className="bg-neutral-100 bg-opacity-30 backdrop-blur-[1.5px] w-full h-36 rounded-b-3xl px-4 pt-4 absolute bottom-0 ">
+                <h2 className="text-white text-2xl font-medium transition-all hover:text-4xl hover:transition-all hover:duration-400 hover:ease-in-out drop-shadow-linkCards">{heading}</h2>
+                <p className="text-white drop-shadow-linkCards">{subscription}</p>
+            </div>
             <img src={img} alt={alt} className={`object-cover rounded-3xl w-full h-full absolute -z-10`} />
         </Link>
-     );
+    );
 }
- 
+
 export default LinkCards;
--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -14,7 +14,7 @@ const Gallery = () => {
                 <article className="grid gap-y-20 mb-14 size1200:grid-cols-3 justify-items-center m-auto lg:w-[80%] size1560:w-[60%] lg:gap-x-20 lg:my-32">
                     <LinkCards url={"/albums"} subscription={"Here is my album covers of musicians and drawings"} heading={"Albums & Drawings"} img={"./images/albums/The Weeknd - Playlist Cover - Red.webp"} alt={"Albums - Page - Link"} />
                     <LinkCards bgGradient={"bg-gradient-to-b from-transparent via-transparent to-black/60"} url={"/schoolprojects"} subscription={"The school projects I am most proud of "} heading={"School Projects"} img={"./images/school/Roskilde - fjorland plakat.webp"} alt={"Drawings - Page - Link"} />
-                    <LinkCards bgGradient={"bg-gradient-to-b from-transparent via-transparent to-black/60"} url={"/posters"} subscription={"Here is my posters and wallpapers of actors and celebrities, etc..."} heading={"Posters & etc..."} img={"./images/posters-wallpapers/Mysterious poster - purple gradient - 7-8-2024.webp"} alt={"Posters and Wallpapers - Page - Link"} />
+                    <LinkCards bgGradient={"bg-gradient-to-b from-transparent via-transparent to-black/60"} url={"/posters"} subscription={"Here is my posters and wallpapers of actors and celebrities, etc..."} heading={"Posters & etc..."} img={"./images/posters-wallpapers/mirror.webp"} alt={"Posters and Wallpapers - Page - Link"} />
                 </article>
                 {/* <h1 className="flex justify-center py-10 lg:py-16 font-semibold underline size200:mx-10 size866:00:mx-0">Sources/items that are used for my designs are personal use sources from the internet, just with some of my own touch ups to create posters or something else.</h1> */}
             </>
