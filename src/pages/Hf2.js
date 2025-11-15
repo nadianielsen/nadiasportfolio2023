8efb1989b206832ed4161b5b2664e50ca77645b5
nadia <nadiamarie2111@gmail.com>
2025-11-15 14:35:19 +0100

    HF2 stuff

---
+++ src/pages/Hf2.js
@@ -0,0 +1,45 @@
+import { Link } from "react-router-dom";
+import DownloadButton from "../components/DownloadButton";
+
+
+const Hf2 = () => {
+    return (
+        <article className="grid gap-y-2">
+            <h1 className="text-3xl py-10 place-self-center">Hovedforløb 2</h1>
+            <section className="flex flex-col size800:flex-row justify-center gap-x-20 gap-y-5 place-self-center mb-4">
+                <div className="text-center size800:text-start m-auto">
+                    <p className="w-[22rem] size800:w-[20.5rem]"><span className="font-semibold">Til hovedforløb 2</span> har jeg valgt to produkter. Til det trykte produkt valgte jeg at lave et magasin, hvor jeg bruger kernefaglighederne - "Grafisk Design", "Typografi og ombrydning" og "Grafisk produktionsforståelse".</p>
+                    <p className="w-[22rem] size800:w-[20.5rem] pt-2">Til det digtale produkt valgte jeg at tage slotmaskine app/spil som jeg lavede nede i Schweiz. Jeg bruger den sidste kernefaglighed - "Grafik og Billedbehandling".</p>
+                    {/* <p className="w-[22rem] size800:w-[20.5rem]"><span className="font-semibold">Til hovedforløb 2</span> har jeg valgt to produkter. Jeg har et trykt produkt som er et magasin med kernefaglighederne - "Grafisk Design", "Typografi og ombrydning" og "Grafisk produktionsforståelse", og et digitalt produkt som er en slotmaskine app med kernefagligheden - "Grafik og billedbehandling".</p> */}
+                    <p className="pt-2">Min arbejdsportfolio ligger trykklar via download knappen.</p>
+                    <DownloadButton filepdf={"/Arbejdsportfolio - Hovedforløb 1 - Nadia Marie Nielsen.pdf"} downloadPDF={"/Arbejdsportfolio - Hovedforløb 1 - Nadia Marie Nielsen"} extraStyle={"my-4 m-auto size800:m-0 size800:my-4"} />
+                </div>
+                <img className="w-80 lg:w-[25rem] rounded-3xl m-auto size800:m-0" src="./images/Arbejdsportfolio - Hovedforløb 2 - Nadia Marie Nielsen.webp" alt="billed af skoleportfolio" />
+            </section>
+            <span className="w-[50%] h-2 border-b-2 border-black place-self-center"></span>
+            <section className="place-self-center">
+                <h2 className="text-2xl pt-6 text-center">Digital Produkt</h2>
+                <img className="w-60 size600:w-80 rounded-3xl m-auto my-10" src="./images/mockup - weuro.webp" alt="mockup - slotmaskine app" />
+                <h3 className="text-center font-semibold">Slotmaskine app</h3>
+                <p className="text-center w-[20rem] size800:w-[25rem]">Her er mit digitale produkt, hvilket er en slotmaskine app, hvor man kan vinde to præmier via UEFA WOMEN'S EURO 2025 i Schweiz.</p>
+                <Link target="_blank" to={"https://www.figma.com/design/e1iVUSGGlCwiXa5faDChM4/Schweiz---projekter?node-id=0-1&p=f"} className="bg-black h-12 w-32 text-white mt-4 m-auto mb-4 rounded-md flex justify-center items-center gap-x-1 hover:bg-transparent hover:border-2 border-black hover:transition hover:text-black hover:duration-500 hover:ease-in-out">Klik på linket</Link>
+                {/* <DownloadButton filepdf={"/Poster-album cover + moving sky background - If I Could.mp4"} downloadPDF={"/Poster-album cover + moving sky background - If I Could"} extraStyle={"mt-4 m-auto mb-4"} /> */}
+            </section>
+            <span className="w-[50%] h-2 border-b-2 border-black place-self-center"></span>
+            <section className="mb-4 place-self-center">
+                <h2 className="text-2xl text-center py-6">Trykt Produkt</h2>
+                <section className="flex flex-col size800:flex-row justify-center items-center size800:items-start gap-x-20 gap-y-4">
+                    <div>
+                        <h3 className="font-semibold text-center size800:text-start">UEFA WOMEN'S EURO 2025 - magasin</h3>
+                        <p className="size800:w-[16rem] text-center size800:text-start w-[20rem]">For mit trykte produkt, valgte jeg at lave et magasin som indebærer info omkring UEFA WOMEN'S EURO 2025 og der ligger en trykklar pdf via knappen. </p>
+                        <DownloadButton filepdf={"/Magasin WEURO - trykt produkt - HF2.pdf"} downloadPDF={"/Magasin WEURO - trykt produkt - HF2"} extraStyle={"mt-4 m-auto size800:m-0 size800:mt-4"} />
+                    </div>
+                    <img className="w-60 size600:w-80 rounded-3xl" src="./images/Magazine Mockup - WEURO.webp" alt="magasin" />
+                </section>
+            </section>
+        </article>
+
+    );
+}
+
+export default Hf2;
