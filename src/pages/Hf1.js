c8cf11023566effbdf8704b5b13f8ea8f506c653
nadia <nadiamarie2111@gmail.com>
2025-12-20 15:32:34 +0100

    changed font, added HF2 pdf etc...

--- src/pages/Hf1.js
+++ src/pages/Hf1.js
@@ -4,7 +4,7 @@ import DownloadButton from "../components/DownloadButton";
 const Hf1 = () => {
     return (
         <article className="grid gap-y-2">
-            <h1 className="text-3xl py-10 place-self-center">Hovedforløb 1</h1>
+            <h1 className="text-3xl py-10 place-self-center ivy_font text-[#7EA46C]">Hoved<span className="italic">forløb</span> 1</h1>
             <section className="flex flex-col size800:flex-row justify-center gap-x-20 gap-y-5 place-self-center mb-4">
                 <div className="text-center size800:text-start m-auto">
                     <p className="w-[22rem] size800:w-[19rem]"><span className="font-semibold">Til mit første hovedforløb</span> har jeg valgt to produkter. Jeg har et trykt produkt som er en plakat med kernefaglighederne - "Grafisk Design" og "Grafik og Billedbehandling" og et digitalt produkt som er en video med kernefaglighederne - "Typografi og ombrydning" og "Grafisk produktionsforståelse".</p>
@@ -15,7 +15,7 @@ const Hf1 = () => {
             </section>
             <span className="w-[50%] h-2 border-b-2 border-black place-self-center"></span>
             <section className="place-self-center">
-                <h2 className="text-2xl pt-6 text-center">Digital Produkt</h2>
+                <h2 className="text-2xl pt-6 text-center ivy_font">Digital Produkt</h2>
                 <video className="w-[20rem] h-[20rem] size700:w-[30rem] size700:h-[30rem] size1100:w-[40rem] size1100:h-[40rem] my-5 m-auto size800:my-10 rounded-3xl" src="./Poster-album cover + moving sky background - If I Could.mp4" controls></video>
                 <h3 className="text-center font-semibold">If I Could - Video/Album Cover/Poster</h3>
                 <p className="text-center w-[20rem] size800:w-full">Her er mit digitale produkt, hvilket består af tekst, 3D tekst og bevægende sky baggrund.</p>
@@ -23,7 +23,7 @@ const Hf1 = () => {
             </section>
             <span className="w-[50%] h-2 border-b-2 border-black place-self-center"></span>
             <section className="mb-4 place-self-center">
-                <h2 className="text-2xl text-center py-6">Trykt Produkt</h2>
+                <h2 className="text-2xl text-center py-6 ivy_font">Trykt Produkt</h2>
                 <section className="flex flex-col size800:flex-row justify-center items-center size800:items-start gap-x-20 gap-y-4">
                     <div>
                         <h3 className="font-semibold text-center size800:text-start">Mother nature is a part of os - plakat/poster</h3>
